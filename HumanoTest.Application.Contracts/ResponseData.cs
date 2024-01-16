a14bf89b2130b446c77d6406ede6c7a52bb98f23
Steven Velez <velez.steven12@gmail.com>
2024-01-16 16:34:02 -0400

    Agregando clase para manejo de respuestas del servidor.

---
+++ HumanoTest.Application.Contracts/ResponseData.cs
@@ -0,0 +1,38 @@
+﻿namespace HumanoTest.Application.Contracts;
+
+/// <summary>
+/// Clase de Respuesta entre el Application y Cliente.
+/// </summary>
+public class ResponseData
+{
+    public ResponseData(bool success, object data)
+    {
+        Success = success;
+        Data = data;
+    }
+
+    public ResponseData(bool success, string message)
+    {
+        Success = success;
+        Message = message;
+    }
+
+    public ResponseData(bool success, string message, int dataId)
+    {
+        Success = success;
+        Message = message;
+        DataId = dataId;
+    }
+
+    public ResponseData()
+    {
+    }
+
+    public bool Success { get; set; }
+
+    public object Data { get; set; }
+
+    public string Message { get; set; }
+
+    public int DataId { get; set; }
+}
