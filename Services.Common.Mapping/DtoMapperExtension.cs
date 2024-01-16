4b6c612bba9634dbd99ffcaf3e1d8ba620322408
Steven Velez <velez.steven12@gmail.com>
2024-01-16 16:32:40 -0400

    Agregando proyectos Common Services Library, para el manejo de paginacion, mapping, dataCollection.

---
+++ Services.Common.Mapping/DtoMapperExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace Services.Common.Mapping;
+
+using System.Text.Json;
+
+public static class DtoMapperExtension
+{
+    public static T MapTo<T>(this object value)
+    {
+        return JsonSerializer.Deserialize<T>(
+            JsonSerializer.Serialize(value)
+        );
+    }
+}
