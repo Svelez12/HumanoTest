2bd0a9af5c1e599fd9c1d1ff8b7a38a024265b09
Steven Velez <velez.steven12@gmail.com>
2024-01-16 16:35:22 -0400

    Agregando Person Service, IGenericQueryService

---
+++ HumanoTest.Application.Contracts/IServices/Generic/IGenericQueryService.cs
@@ -0,0 +1,10 @@
+﻿namespace HumanoTest.Application.Contracts.IServices.Generic;
+
+public interface IGenericQueryService
+{
+    /// <summary>
+    /// Obtener todos los registros.
+    /// </summary>
+    /// <returns> <see cref="ResponseData"/> </returns>
+    Task<ResponseData> GetAllAsync(int page, int take);
+}
---
+++ HumanoTest.Application.Contracts/IServices/Person/IPersonService.cs
@@ -0,0 +1,22 @@
+﻿namespace HumanoTest.Application.Contracts.IServices.Person;
+
+using HumanoTest.Application.Contracts.IServices.Generic;
+using HumanoTest.Application.Models.Person;
+
+public interface IPersonService : IGenericQueryService
+{
+    /// <summary>
+    /// Crear <see cref="Person"/>.
+    /// </summary>
+    /// <param name="PersonDto"><see cref="PersonCreateDto"/>. </param>
+    /// <returns> <see cref="ResponseData"/> </returns>
+    Task<ResponseData> CreateAsync(PersonCreateDto personCreateDto);
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="identityNumberId"></param>
+    /// <returns></returns>
+    Task<ResponseData> GetByIdentityNumberId(int page, int take, int identityNumberId);
+}
