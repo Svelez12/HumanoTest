3214f5327690ca4809ca7e8c1ec60f1fdef63aea
Steven Velez <velez.steven12@gmail.com>
2024-01-16 16:28:51 -0400

    Agregando IoC Services Register y HumanoTest.CrossCutting

---
+++ HumanoTest.CrossCutting/Register/IoCServicesRegister.cs
@@ -0,0 +1,12 @@
+﻿namespace HumanoTest.CrossCutting.Register;
+
+using HumanoTest.CrossCutting.Register.Person;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class IoCServicesRegister
+{
+    public static void AddRegistration(this IServiceCollection services)
+    {
+        IoCPersonServicesRegister.AddRegistration(services);
+    }
+}
---
+++ HumanoTest.CrossCutting/Register/Person/IoCPersonRepositoriesRegister.cs
@@ -0,0 +1,24 @@
+﻿namespace HumanoTest.CrossCutting.Register.Person;
+
+using HumanoTest.Domain.Contracts.IRepositories.Person;
+using HumanoTest.Infrastructure.Repositories.Person;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class IoCPersonRepositoriesRegister
+{
+    public static void AddRegistration(this IServiceCollection services)
+    {
+        AddRegisterRepositories(services);
+    }
+
+    /// <summary>
+    /// Agregar Registro de Repositorios.
+    /// </summary>
+    /// <param name="services"> Repositorio. </param>
+    private static void AddRegisterRepositories(IServiceCollection services)
+    {
+        //Person
+        services.AddScoped<IPersonRepository, PersonRepository>();
+        services.AddScoped<IPersonContactRepository, PersonContactRepository>();
+    }
+}
